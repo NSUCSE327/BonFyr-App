8341e64d2a24b5113f917a217d2da48338a68fd3
ayon <sadia.afrin17@northouth.edu>
2021-01-12 10:58:16 +0600

    style crud pages and add boilerplate

--- models/campground.js
+++ models/campground.js
@@ -3,11 +3,12 @@ const Schema = mongoose.Schema;
 
 /**
  * Campground schema
- * @type {{title: String, price: String, description: String, location: String}}
+ * @type {{title: String, image: String, price: Number, description: String, location: String}}
  */
 const CampgroundSchema = new Schema({
     title: String,
-    price: String,
+    image: String,
+    price: Number,
     description: String,
     location: String
 });
